dad6695b4e49d71f02b31194e49732c4a985508d
Jan van Gelder <43755492+JvGldr@users.noreply.github.com>
2020-05-15 14:09:27 +0200

    Update config.js

--- config.js
+++ config.js
@@ -58,11 +58,11 @@ var respecConfig =
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
   //publishDate: "2019-10-08",  	    // Format is "YYYY-MM-DD"
   //-- Repositorynaam op GitHub -------------------------------------------------------
-  github: "https://github.com/geonovum/disgeo-architectuur",
+  github: "https://github.com/geonovum/disgeo-arch",
   //-- Repositorynaam/issues op GitHub ------------------------------------------------
-  issueBase: "https://github.com/geonovum/disgeo-architectuur/issues",
+  issueBase: "https://github.com/geonovum/disgeo-arch/issues",
   //-- edDraftURI: de URI van de werkversie van het document
-  edDraftURI: "https://github/geonovum/disgeo-architectuur",
+  edDraftURI: "https://github/geonovum/disgeo-arch",
   //-- de namen van de Editor(s) ------------------------------------------------------
   //-- vul in: per Editor: name:, company:, companyURL: -------------------------------
   editors: 
